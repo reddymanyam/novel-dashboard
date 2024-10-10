bc1a0377be65e4b7163bd35da2c21bf7f1524dbf
reddymanyam <reddappareddy18@gmail.com>
2024-10-10 22:27:30 +0530

    updated

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,6 +7,12 @@ import profilepic from '../../assets/profile_icon.png';
 import person from '../../assets/person.png';
 import Box from '@mui/material/Box';
 import profile from '../../assets/profile.png';
+import adugodi from '../../assets/Frame 1000002926 (2).png';
+import whitefield from '../../assets/Frame 1000002926 (4).png';
+import mgroad from '../../assets/Frame 1000002926 (1).png';
+import marthalli from '../../assets/Frame 1000002924.png';
+import queensroad from '../../assets/Frame 1000002926 (3).png';
+import kudluroad from '../../assets/Frame 1000002926.png';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
@@ -27,6 +33,12 @@ const CustomImage = styled('img')({
   objectFit: "cover"
 })
 
+const CustomImage1 = styled('img')({
+  width: '100%',
+  height:"100%",
+  borderRadius:"15px"
+})
+
 const CustomItem = styled(Item)({
   backgroundColor: "#e8f6ff", height: "200px", borderRadius: "15px"
 })
@@ -34,17 +46,17 @@ export default function FullWidthGrid() {
   return (
     <Stack sx={{ flexGrow: 1, width: "100%", padding: "30px" }}>
       <Grid container spacing={2} >
-        <Grid size={{ xs: 6, md: 12 }} >
+        <Grid size={{ xs: 12, md: 12 }} >
           <Item sx={{ bgcolor: "#e8f6ff", height: "60px", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
             <CustomImage src={profilepic} alt='profilepic' />
             <Typography variant="h6" component="h2" color='black'>Welcome Sample</Typography>
             <img src={person} alt='person' style={{ height: "100%", marginLeft: "auto" }} />
           </Item>
         </Grid>
-        <Grid size={{ xs: 6, md: 12 }}>
-          <Item sx={{ bgcolor: "#e8f6ff", height: "120px", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-            <img src={profile} alt='person' style={{ height: "100%", marginLeft: "30px" }} />
-            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
+        <Grid size={{ xs: 12, md: 12 }}>
+          <Item sx={{ bgcolor: "#e8f6ff", height: "120px", display: "flex", flexDirection: "row",  alignItems: "center" }}>
+            <img src={profile} alt='person' style={{ height: "100%", marginLeft: "4%" }} />
+            <Box display="flex" flexDirection="column" alignItems="center"  marginLeft="27%" >
               <Typography variant="h5" component="h2" color='black'>Facing Issues?</Typography>
               <Button variant="contained" size='small'>Raise a Ticket </Button>
             </Box>
@@ -52,26 +64,38 @@ export default function FullWidthGrid() {
         </Grid>
       </Grid>
       <Typography variant="h6" component="h2">Featured listings</Typography>
-      <Stack sx={{ flexGrow: 1, width: "100%" }}>
-
+    
+      <Stack >
         <Grid container spacing={2} >
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem></CustomItem>
+            <CustomItem>
+            <CustomImage1 src={adugodi} alt='person'  />
+            </CustomItem>
           </Grid>
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem ></CustomItem>
+            <CustomItem >
+            <CustomImage1 src={whitefield} alt='person'  />
+            </CustomItem>
           </Grid>
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem ></CustomItem>
+            <CustomItem >
+            <CustomImage1 src={mgroad} alt='person'  />
+            </CustomItem>
           </Grid>
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem ></CustomItem>
+            <CustomItem >
+            <CustomImage1 src={marthalli} alt='person'  />
+            </CustomItem>
           </Grid>
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem ></CustomItem>
+            <CustomItem >
+            <CustomImage1 src={queensroad} alt='person'  />
+            </CustomItem>
           </Grid>
           <Grid size={{ xs: 6, md: 4 }} >
-            <CustomItem ></CustomItem>
+            <CustomItem >
+            <CustomImage1 src={kudluroad} alt='person'  />
+            </CustomItem>
           </Grid>
 
         </Grid>
