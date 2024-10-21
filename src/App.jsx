5f0856534c896d1a955871bb883e40a2cc0c18d8
reddymanyam <reddappareddy18@gmail.com>
2024-10-21 11:07:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,10 @@ import router from './Routes/Routes'
 const App = () => {
   const routing = useRoutes(router);
   return (
-<<<<<<< HEAD
-   <>
-   <Layout />
-   </>
-=======
+
     <>
       {routing}
     </>
->>>>>>> 5ddd4feaa5d43d3083f99a2acb83dd517231a04f
   )
 }
 
