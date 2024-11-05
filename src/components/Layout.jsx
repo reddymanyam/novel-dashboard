435ad2cf363e338868fba0bd7acbfda2da0d985b
reddymanyam <reddappareddy18@gmail.com>
2024-11-05 22:58:00 +0530

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({toggleTheme, mode}) => {
             <Box  height="50px" position="fixed" width="calc(100% - 210px)" display='flex' justifyContent="flex-start" marginLeft="210px" top="0">
                 <Navbar toggleTheme={toggleTheme} mode={mode} />
             </Box>
-            <Box display='flex' justifyContent="center" aliginItems="center" height="100vh" width="210px" position='fixed' overflowY="scroll" top="0px">
+            <Box display='flex' justifyContent="center" aliginItems="center" height="100vh" width="210px" position='fixed' overflowY="scroll" top="50px">
                 <Sidebar />
             </Box>
             <Box display='flex' justifyContent="center" aliginItems="center" overflow="auto" width='calc(100% - 210px)' height="calc(100% - 50px)"  padding="30px"  marginLeft="210px" top="35px" >
