02e81bfd7b1ca67a67f743fb0d48f6407cf5ea9d
reddymanyam <reddappareddy18@gmail.com>
2024-11-10 23:55:07 +0530

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,18 +5,14 @@ import Sidebar from './Sidebar'
 import { Box } from '@mui/material'
 
 const Layout = ({toggleTheme, mode}) => {
-
-    const handleClick = () =>{
-
-    } 
   return (
     <>
         <Box display="flex" width="100%" height="100vh">
             <Box  height="50px" position="fixed" width="calc(100% - 210px)" display='flex' justifyContent="flex-start" marginLeft="210px" top="0">
-                <Navbar toggleTheme={toggleTheme} mode={mode} handleClick={handleClick}/>
+                <Navbar toggleTheme={toggleTheme} mode={mode} />
             </Box>
             <Box display='flex' justifyContent="center" aliginItems="center" height="100vh" width="210px" position='fixed' overflowY="scroll" top="0px">
-                <Sidebar handleClick={handleClick} />
+                <Sidebar />
             </Box>
             <Box display='flex' justifyContent="center" aliginItems="center" overflow="auto" width='calc(100% - 210px)' height="calc(100% - 50px)"  padding="30px"  marginLeft="210px" top="35px" >
                 <Outlet />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Box from '@mui/material/Box';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
@@ -10,27 +10,27 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 const CustomImage = styled('img')({
-  width:'30px',
-  height:"30px",
-  borderRadius:"50%",
-  marginLeft:'5px',
-  objectFit:"cover"
-})
+  width: '30px',
+  height: "30px",
+  borderRadius: "50%",
+  marginLeft: '5px',
+  objectFit: "cover"
+});
 
-const Navbar = ({toggleTheme, mode, handleClick}) => {
+const Navbar = ({ toggleTheme, mode, handleClick }) => {
   return (
-   <Box width="100%" display='flex' justifyContent="space-between" alignItems="center" padding="10px 20px">
-       <ReorderIcon onClick={handleClick}/>
-       <Box display="flex" justifyContent="flex-end" alignItems="center">
-       <IconButton onClick={toggleTheme} >
+    <Box width="100%" display='flex' justifyContent="space-between" alignItems="center" padding="10px 20px">
+      <ReorderIcon onClick={handleClick} />
+      <Box display="flex" justifyContent="flex-end" alignItems="center">
+        <IconButton onClick={toggleTheme}>
           {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
         </IconButton>
-        <HelpOutlineOutlinedIcon  sx={{marginRight:"15px"}}/>
-        <NotificationsActiveOutlinedIcon sx={{marginRight:"15px"}} />
+        <HelpOutlineOutlinedIcon sx={{ marginRight: "15px" }} />
+        <NotificationsActiveOutlinedIcon sx={{ marginRight: "15px" }} />
         <CustomImage src={profilepic} alt='profilepc' />
-       </Box>
-   </Box>
-  )
-}
+      </Box>
+    </Box>
+  );
+};
 
-export default Navbar
+export default Navbar;
